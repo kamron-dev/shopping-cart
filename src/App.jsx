d3e3cd188d11668e2abd74257d844718d809091d
kamron-dev <k.kamron@yadnex.ru>
2025-05-27 16:59:20 +0500

    added the new Link buttons, before adding Outlet element

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { Link } from "react-router-dom";
 import './App.css'
 
 function App() {
   return (
     <>
       <header>
-        <h3>Name of the site</h3>
+        <Link to="/">
+          <h3>StockY</h3>
+        </Link>
         <input type="search" name="search-bar" id="search-bar" placeholder='Enter name...' />
         <button>Search</button>
+        <Link to="shop">Shop</Link>
         <button>Cart placeholder</button>
       </header>
       <main>
