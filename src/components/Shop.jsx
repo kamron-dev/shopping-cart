5c4ea1085cedb0d1452d80c7999bde29a79fee92
kamron-dev <k.kamron@yadnex.ru>
2025-05-29 11:07:54 +0500

    still figuring out

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,35 @@
+import { useOutletContext } from "react-router-dom";
+
+const ShopItem = (data) => {
+    return (
+        <div>
+            <h3>{data.title}</h3>
+            <p>{data.description}</p>
+        </div>
+    );
+}
+
 const Shop = () => {
-    return <h1>Hello, Shop!</h1>
+    const data = useOutletContext();
+    
+//     if (!Array.isArray(data)) {
+//     return <p>Loading shop...</p>; // or skeleton, spinner, etc.
+//   }
+    
+    return (
+        <div>
+            <h1>Hello, Shop!</h1>
+            {
+                data ? 
+                <div>
+                    {data.map(item => <ShopItem data={item} key={item.id} />)}
+               
+                
+                
+                </div> : <h1>Loading...</h1>
+            }
+        </div>
+    );
 };
 
 export default Shop;
