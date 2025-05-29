10e42ca9274fdb7361d6aa26d436d7c20879bb59
kamron-dev <k.kamron@yadnex.ru>
2025-05-29 14:57:43 +0500

    separate footer component added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+const Footer = () => {
+    return (
+        <div id="footer-div-info">
+            <h3>Company</h3>
+            <h3>Help</h3>
+            <h3>FAQ</h3>
+            <p>About </p>
+            <p>Customer support </p>
+            <p>Account </p>
+            <p>Features </p>
+            <p>Delivery </p>
+            <p>Manage Delivery</p>
+        </div>
+    )
+};
+
+export default Footer;
