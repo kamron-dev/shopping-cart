6aabe1e8ff06c978d83bd3e374a05bfd01eceb5f
kamron-dev <k.kamron@yadnex.ru>
2025-05-30 14:18:35 +0500

    added some css

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 
 const GameCard = ({ data }) => {
     return (
+        
         <div id="game-card">
+            
+            <img src={data.image} alt="shop-item-photo" />
             <Link to={`/shop/${data.id}`}>
-                <img src={data.image} alt="shop-item-photo" />
                 <h3>{data.title}</h3>
                 <p>${data.price}</p>
+            
             </Link>
             
         </div>
+
+        
     );
 };
 
