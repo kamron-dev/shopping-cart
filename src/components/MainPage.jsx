20afb07bc3a33c235eea95a3dc22c974bce655b7
kamron-dev <k.kamron@yadnex.ru>
2025-06-03 15:38:27 +0500

    cleaned the main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,17 +1,14 @@
-import { useOutletContext } from "react-router-dom";
+// import { useOutletContext } from "react-router-dom";
 
 const MainPage = () => {
-    const { cart } = useOutletContext();
-    console.log(cart)
+    
     return (
         <>
             <div id="banner">
                 <h1>Welcome to the StockY shop!</h1>
             </div>
             <div id="top-picks">
-                <div>
-                    {cart.map((item) => <p key={item.id}>{item.title}</p>)}
-                </div>
+                
             </div>
         </>
     )
