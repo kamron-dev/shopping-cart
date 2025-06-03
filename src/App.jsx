0f48c51fe5fbbed5cbfbb3a38a917349dc3356a6
kamron-dev <k.kamron@yadnex.ru>
2025-06-03 10:39:49 +0500

    deleted unused imports of logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { Link, Outlet } from "react-router-dom";
 import './App.css';
 import Footer from "./components/Footer";
