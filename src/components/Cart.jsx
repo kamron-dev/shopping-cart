ed35e7fb2b514fe1f5dda1c0c89be664c6a643ab
kamron-dev <k.kamron@yadnex.ru>
2025-06-03 12:12:48 +0500

    cart starts looking like a cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,36 @@
-const Cart = ({info}) => {
-    if (!info) return <h2>Your cart is empty...</h2>
+import { useOutletContext } from "react-router-dom";
+
+
+const Cart = () => {
+    const { cart } = useOutletContext();
+    if (!cart.length) return <h2>Your cart is empty...</h2>
+    
+    
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {cart.map((item) => {
+                    return (
+                        <tr>
+                            <td>
+                                {item.title}
+                            </td>
+                            <td>
+                                ${item.price}
+                            </td>
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
+    )
+     
 };
 
 export default Cart;
