e5489e957cde53e60e438a072f17e09d9d37735c
kamron-dev <k.kamron@yadnex.ru>
2025-06-11 14:46:59 +0500

    filters working

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,27 +2,58 @@ import { useState, useEffect } from "react";
 import GameCard from "./GameCard";
 import { useSearchParams } from "react-router-dom"; 
 
+const FilterBy = ({data, handleChange}) => {
+    return (
+        <div>
+            <label htmlFor="category-filter">Filter by Category</label>
+            <select name="category-filter" id="category-filter" onChange={handleChange}>
+                {
+                    [...new Set(data.map(item => item.category))].map(item => <option value={item} key={item}>{item }</option>)
+                }
+            </select>
+        </div>
+    )
+}
+
 const Shop = () => {
     const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     const [searchParameter] = useSearchParams();
     const query = searchParameter.get("q") || "";
 
-
     useEffect(() => {
         fetch("https://fakestoreapi.com/products", { mode: "cors" })
             .then((response) => response.json())
-            .then((response) => setData(response))
+            .then((response) => {
+                setData(response);
+                setFilteredData(response);
+            })
             .catch((error) => setError(error))
             .finally(() => setLoading(false));
     }, []);
 
-    const filteredData = query
-        ? data.filter(item =>
+    const handleCategoryChange = (e) => {
+        const filter = e.target.value;
+        if (!filter) {
+            setFilteredData(data)
+        } else {
+            const filteredData = data.filter(item => item.category === filter);
+            setFilteredData(filteredData);
+
+        };
+
+    }
+
+
+    const searchFilteredData = query
+        ? filteredData.filter(item =>
             item.title.toLowerCase().includes(query)
-        ) : data;
+        ) : filteredData;
+    
+    
     
     
     if (loading) return <h2>Loading...</h2>
@@ -31,11 +62,17 @@ const Shop = () => {
     
     return (
         <div id="shop-container">
-            {/* <h1>Hello, Shop!</h1> */}
+            <FilterBy data={data} handleChange={handleCategoryChange}/>
             {
                 <div id="shop-cards-container">
-                    {filteredData.map(item => <GameCard data={item} key={item.id} />)}
-               
+                    {searchFilteredData.map(item => <GameCard data={item} key={item.id} />)}
+                    {searchFilteredData.length === 0 ? (
+                        <h2>No items matching your filter</h2>
+                    ) : (
+                            searchFilteredData.map(item => (
+                                <GameCard data={item} key={item.id} />
+                            ))
+                    )}
                 
                 
                 </div>
